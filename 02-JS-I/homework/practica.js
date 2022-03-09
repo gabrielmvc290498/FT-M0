6ef7e88326c200dc46282b25db0801b131f803d8
gabrielmvc290498 <gvilcahuamanc@gmail.com>
2022-03-09 11:29:18 -0500

    practicando

---
+++ 02-JS-I/homework/practica.js
@@ -0,0 +1,7 @@
+var resultado = 100
+function verificar(num){
+    if(num===100) return "es igual a 100";
+    return "es diferente de 100";
+}
+console.log(verificar(200));
+console.log(verificar(100));
