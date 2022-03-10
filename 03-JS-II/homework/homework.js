96b38ffe1c566730bc05aef94371e29eaa3629e3
gabrielmvc290498 <gvilcahuamanc@gmail.com>
2022-03-09 19:53:19 -0500

    avance fprefinal2Â

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -204,7 +204,6 @@ function esVerdadero(valor){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
 }
 
 function doWhile(numero) {
